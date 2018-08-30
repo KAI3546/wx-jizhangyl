761cf4a6f1a62a1d6e2921e3278f6c83c1abe491
KAI <huangkai@jizhangyl.com>
2018-08-30 10:59:57 +0800

    第一次提交代码2018年8月30日10:59:47

---
+++ src/utils/constant.js
@@ -0,0 +1,26 @@
+/**
+ * 用户code 换取 session_key
+ * @type {String}
+ */
+export const USER_SPECICAL_INFO = "userSpecialInfo";
+
+/**
+ * 用户信息
+ * @type {String}
+ */
+export const USER_INFO = "userInfo";
+
+/**
+ * 系统信息
+ * @type {String}
+ */
+export const SYSTEM_INFO = "systemInfo";
+
+
+export const ADDRESS_ID = "addressId";
+
+export const SEL_CLASS_CODE = "selClassCode";
+
+export const HOST_URL = "https://www.jizhangyl.com/jizhangyl/";
+
+export const IMG_URL = "";
--- wepy.config.js
+++ wepy.config.js
@@ -31,7 +31,7 @@ module.exports = {
         'transform-class-properties',
         'transform-decorators-legacy',
         'transform-object-rest-spread',
-        'transform-export-extensions',
+        'transform-export-extensions'
       ]
     }
   },
